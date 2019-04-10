e226ed0b355207cbb89ca13b14fffa3e0bc72b11
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-04-10 14:59:10 -0700

    Make a copy of the config area

--- deploy/docker/osg-csi-cvmfsplugin-startup.sh
+++ deploy/docker/osg-csi-cvmfsplugin-startup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# OSG cvmfs driver needs /cvmfs/config-osg.opensciencegrid.org mounted
 mount -t cvmfs config-osg.opensciencegrid.org /cvmfs/config-osg.opensciencegrid.org
 if [ $? -ne 0 ]; then
   echo "Failed to mount config-osg, ABORTING"
   exit 1
 fi
+
+# However, we cannot keep it mounted in the CSI module, so make a copy of the essential pieces
+mkdir -p /cvmfs/config-osg.opensciencegrid.org.copy
+(cd /cvmfs/config-osg.opensciencegrid.org && tar -cf - etc/cvmfs) | (cd /cvmfs/config-osg.opensciencegrid.org.copy && tar -xf -)
+umount /cvmfs/config-osg.opensciencegrid.org
+mv /cvmfs/config-osg.opensciencegrid.org.copy /cvmfs/config-osg.opensciencegrid.org
+
+# Now we can launch the real CSI process
 exec /csi-cvmfsplugin "$@"
