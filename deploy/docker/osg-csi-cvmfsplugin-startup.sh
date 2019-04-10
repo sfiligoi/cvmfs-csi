1b45518af8362adfe5cfbe03a20933cd5c0c125e
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-04-10 15:42:46 -0700

    Bug fix

--- deploy/docker/osg-csi-cvmfsplugin-startup.sh
+++ deploy/docker/osg-csi-cvmfsplugin-startup.sh
@@ -9,8 +9,14 @@ fi
 
 # However, we cannot keep it mounted in the CSI module, so make a copy of the essential pieces
 mkdir -p /cvmfs/config-osg.opensciencegrid.org.copy
-(cd /cvmfs/config-osg.opensciencegrid.org && tar -cf - etc/cvmfs) | (cd /cvmfs/config-osg.opensciencegrid.org.copy && tar -xf -)
-umount /cvmfs/config-osg.opensciencegrid.org
+(cd /cvmfs/config-osg.opensciencegrid.org && tar -cf - .) | (cd /cvmfs/config-osg.opensciencegrid.org.copy && tar -xf -)
+
+umount /cvmfs/config-osg.opensciencegrid.org && rmdir /cvmfs/config-osg.opensciencegrid.org
+if [ $? -ne 0 ]; then
+  echo "Failed to umount config-osg, ABORTING"
+  exit 1
+fi
+
 mv /cvmfs/config-osg.opensciencegrid.org.copy /cvmfs/config-osg.opensciencegrid.org
 
 # Now we can launch the real CSI process
